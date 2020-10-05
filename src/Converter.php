e3169d2181c8802c3eede6e6f59b9dc2a8a13ef5
Norbert Papp <thepianistman@gmail.com>
2020-10-05 07:39:01 +0200

    KATA-01 - arabic to roman converter
    Refactoring #1

--- src/Converter.php
+++ src/Converter.php
@@ -6,23 +6,51 @@ class Converter
     {
         $roman = '';
 
-        if ($arabic >= 1000) {
-            $roman = 'M';
-        } elseif ($arabic >= 500) {
-            $roman = 'D';
-        } elseif ($arabic >= 100) {
-            $roman = 'C';
-        } elseif ($arabic >= 50) {
-            $roman = 'L';
-        } elseif ($arabic >= 10) {
-            $roman = 'X';
-        } elseif ($arabic >= 5) {
-            $roman = 'V';
+        if ($arabic > 999) {
+            while ($arabic > 999) {
+                $roman .= $roman . 'M';
+                $arabic -= 1000;
+            }
+        }
+
+        if ($arabic > 499) {
+            while ($arabic > 499) {
+                $roman .= $roman . 'D';
+                $arabic -= 500;
+            }
+        }
+
+        if ($arabic > 99) {
+            while ($arabic > 99) {
+                $roman .= $roman . 'C';
+                $arabic -= 100;
+            }
+        }
+
+        if ($arabic > 49) {
+            while ($arabic > 49) {
+                $roman .= $roman . 'L';
+                $arabic -= 50;
+            }
+        }
+
+        if ($arabic > 9) {
+            while ($arabic > 9) {
+                $roman .= $roman . 'X';
+                $arabic -= 10;
+            }
+        }
+
+        if ($arabic > 4) {
+            while ($arabic > 4) {
+                $roman .= $roman . 'V';
+                $arabic -= 5;
+            }
         }
 
         if ($arabic < 5) {
-            for ($i = 0; $i < $arabic; $i++) {
-                $roman .= 'I';
+            for ($i = $arabic; $i > 0; $i--) {
+                $roman = $roman . 'I';
             }
         }
 
